d09cf8bfa5aef4e441b5575834a2e6707cb672e8
estong jamion <estong.jamion@gmail.com>
2022-12-23 08:10:35 +0300

    updated frontend link

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://my-portfolio-a691.onrender.com"],
+    origin: ["http://localhost:3000", "https://my-portfolio-avkf.onrender.com"],
   })
 );
 
